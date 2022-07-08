9ecfd836bd45afb52a6b65e83fd98338cda5b032
natelac <natelac@umich.edu>
2022-07-07 20:40:15 -0400

    Add commented code for checking guild member statuses

--- bot.py
+++ bot.py
@@ -13,4 +13,17 @@ client = discord.Client()
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
 
+# @client.event
+# async def on_presence_update(before, after):
+#     if before.status != after.status:
+#         print(
+#             f'User {before.name} has changed their status'
+#             f' from {before.status} to {after.status}'
+#     else:
+#         print(
+#             f'User {before.name} has changed their activity'
+#             f' from {before.activity} to {after.activity}'
+
 client.run(TOKEN)
+
+
